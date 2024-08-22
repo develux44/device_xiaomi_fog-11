a594b07b0a7ce599b22862056c5e6c2f8815e8f5
develux44 <erroor234@gmail.com>
2024-08-22 18:12:49 +0500

    Device mk

--- device.mk
+++ device.mk
@@ -368,10 +368,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS := # leave it empty
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Perf
-PRODUCT_PACKAGES += \
-    libqti-perfd-client
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
