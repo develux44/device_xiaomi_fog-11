19648104f839f587150586c49c607e39b3ad75aa
develux44 <erroor234@gmail.com>
2025-02-04 18:05:19 +0500

    Vendor property to enable fetching of QoS parameters via IQtiRadio HAL

--- device.mk
+++ device.mk
@@ -562,6 +562,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
+# Vendor property to enable fetching of QoS parameters via IQtiRadio HAL
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.fetchqos=true
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
